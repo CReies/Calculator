4b0343f339cfd06e4f68d2d09f0aa8d4882a7d1d
Cristian Reyes <ccreyesmolina@gmail.com>
2023-07-28 20:54:21 -0500

    the calculator is functional

--- js/main.js
+++ js/main.js
@@ -1,26 +1,72 @@
 const result = document.querySelector('#result');
-const operation = {
-	val1: null,
-	val2: null,
-	operation: null,
-	result: null,
+const operation = {};
+
+const setValues = val => {
+	if (!operation.operation) {
+		if (!operation.value1) operation.value1 = val;
+		else operation.value += val;
+	} else {
+		if (!operation.value2) operation.value2 = val;
+		else operation.value += val;
+	}
+};
+
+const resolve = () => {
+	switch (operation.operation) {
+		case '+':
+			operation.result = Number(operation.value1) + Number(operation.value2);
+			break;
+		case '-':
+			operation.result = Number(operation.value1) - Number(operation.value2);
+			break;
+		case '*':
+			operation.result = Number(operation.value1) * Number(operation.value2);
+			break;
+		case '/':
+			operation.result = Number(operation.value1) / Number(operation.value2);
+			break;
+	}
+	operation.value1 = operation.result;
+	operation.value2 = undefined;
+	operation.operation = undefined;
+};
+
+const setOperation = val => {
+	if (!operation.operation) {
+		operation.operation = val;
+		return;
+	}
+	resolve();
+	operation.operation = val;
 };
 
 const write = val => {
 	switch (val) {
 		case 'C':
 			result.innerHTML = '';
+			operation.value1 = undefined;
+			operation.value2 = undefined;
+			operation.operation = undefined;
+			operation.result = undefined;
 			break;
 		case '=':
-			result.innerHTML += `<br /> =${operation.result}`;
+			resolve();
+			if (operation.result) result.innerHTML += `=<br />${operation.result}`;
+			operation.result = undefined;
+			break;
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+			setOperation(val);
+			result.innerHTML += val;
 			break;
 		default:
+			setValues(val);
 			result.innerHTML += val;
 			break;
 	}
 };
-
-const resolve = () => {};
 
 document.querySelectorAll('.btn').forEach(el => {
 	el.addEventListener('click', () => write(el.value), false);
